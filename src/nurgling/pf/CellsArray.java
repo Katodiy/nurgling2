23dbace21e942f22b7ca5f0a01add373c93bae35
Dmitry <ddm@home.com>
2024-01-03 16:21:27 +0300

    Fixed CellsArray and introduced AreaD optimisation.

--- src/nurgling/pf/CellsArray.java
+++ src/nurgling/pf/CellsArray.java
@@ -3,7 +3,8 @@ package nurgling.pf;
 import haven.*;
 import nurgling.*;
 
-public class CellsArray {
+public class CellsArray
+{
     public Coord begin;
     public Coord end;
     public short[][] cells;
@@ -11,12 +12,14 @@ public class CellsArray {
     public int x_len;
     public int y_len;
 
-    public CellsArray(Gob gob, byte scale) {
+    public CellsArray(Gob gob, byte scale)
+    {
         this(gob.ngob.hitBox, gob.a, gob.rc, scale);
     }
 
 
-    public CellsArray(NHitBox hb, double angl, Coord2d rc, byte scale) {
+    public CellsArray(NHitBox hb, double angl, Coord2d rc, byte scale)
+    {
 
 //        if ((Math.abs((angl * 180 / Math.PI) / 90) % 1 < 0.01)) {
 //            Coord2d a1 = hb.begin.rotate(angl).shift(rc);
@@ -33,30 +36,28 @@ public class CellsArray {
 //                }
 //            }
 //        } else {
-            AreaD objToApproach = new AreaD(hb.end, hb.begin, rc, angl);
-            begin = Utils.toPfGrid(objToApproach.getCircumscribedUL(), scale);
-            Coord2d dBegin = Utils.pfGridToWorld(begin, scale);
-            end = Utils.toPfGrid(objToApproach.getCircumscribedBR(), scale);
-            x_len = end.x - begin.x + 1;
-            y_len = end.y - begin.y + 1;
-            cells = new short[x_len][y_len];
-            Coord2d pos = Coord2d.of(dBegin.x, dBegin.y);
-            for (int i = 0; i < x_len; i++) {
-                for (int j = 0; j < y_len; j++) {
-                    AreaD tile = new AreaD(pos, pos.add(MCache.tilehsz));
-                    cells[i][j] = 0;
-                    for (int k = 0; k < 4; k++) {
-                        if (tile.containsGreedy(objToApproach.c[k]) || objToApproach.containsGreedy(tile.c[k])) {
-                            cells[i][j] = (short) 1;
-                        }
-
+        AreaD objToApproach = new AreaD(hb.end, hb.begin, rc, angl);
+        begin = Utils.toPfGrid(objToApproach.getCircumscribedUL(), scale);
+        Coord2d dBegin = Utils.pfGridToWorld(begin, scale).sub(MCache.tileqsz);
+        end = Utils.toPfGrid(objToApproach.getCircumscribedBR(), scale);
+        x_len = end.x - begin.x + 1;
+        y_len = end.y - begin.y + 1;
+        cells = new short[x_len][y_len];
+        Coord2d pos = Coord2d.of(dBegin.x, dBegin.y);
+        for (int i = 0; i < x_len; i++)
+        {
+            for (int j = 0; j < y_len; j++)
+            {
+                AreaD tile = new AreaD(dBegin.add(MCache.tilehsz.mul(i, j)), pos.add(MCache.tilehsz.mul(i + 1, j + 1)));
+                cells[i][j] = 0;
+                for (int k = 0; k < 4; k++)
+                {
+                    if (tile.containsLoosely(objToApproach.c[k]) || objToApproach.containsLoosely(tile.c[k]))
+                    {
+                        cells[i][j] = (short) 1;
                     }
-                    pos.y += (MCache.tilehsz.y);
                 }
-                pos.x += (MCache.tilehsz.x);
-                pos.y = dBegin.y;
             }
-
-//        }
+        }
     }
 }
