3d9016741e3d120a325be6e45b32257a5a265610
Katodiy <katodiy@yandex.ru>
2024-10-16 03:02:03 +0300

    WaitGobModelAttr rollback

--- src/nurgling/tasks/WaitGobModelAttr.java
+++ src/nurgling/tasks/WaitGobModelAttr.java
@@ -7,7 +7,6 @@ import nurgling.NUtils;
 public class WaitGobModelAttr implements NTask {
     Gob gob;
     int flag;
-    int count =0;
     public WaitGobModelAttr(Gob gob, int flag) {
         this.gob = gob;
         this.flag = flag;
@@ -16,9 +15,7 @@ public class WaitGobModelAttr implements NTask {
 
     @Override
     public boolean check() {
-        count++;
-        return (((gob.ngob!=null) && (gob.ngob.getModelAttribute()&flag)!=0)) || count>100;
-
+        return ((gob.ngob!=null) && (gob.ngob.getModelAttribute()&flag)!=0);
     }
 
 }
