3aa3e2259b1b7001dcc5f007adaa3d5779081cb1
Katodiy <katodiy@yandex.ru>
2024-10-19 15:59:11 +0300

    alarm manager< dir alarm

--- src/nurgling/widgets/NAlarmWdg.java
+++ src/nurgling/widgets/NAlarmWdg.java
@@ -1,20 +1,28 @@
 package nurgling.widgets;
 
 import haven.*;
+import haven.res.ui.obj.buddy.Buddy;
+import nurgling.NAlarmManager;
 import nurgling.NStyle;
 import nurgling.NUtils;
+import nurgling.overlays.NDirArrow;
 import nurgling.tools.Finder;
 import nurgling.tools.NAlias;
 import nurgling.tools.NParser;
 
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import static nurgling.overlays.NBarrelOverlay.bsans;
 
 public class NAlarmWdg extends Widget
 {
     final public static ArrayList<Long> borkas = new ArrayList();
-    ArrayList<Long> alarms = new ArrayList<>();
-
-
+    final ArrayList<Long> alarms = new ArrayList<>();
+    private static final Text.Furnace active_title = new PUtils.BlurFurn(new Text.Foundry(bsans, 15, Color.WHITE).aa(true), 2, 1, new Color(36, 25, 25));
+    TexI numberAlarm = null;
     public NAlarmWdg() {
         super();
         sz = NStyle.alarm[0].sz();
@@ -45,7 +53,71 @@ public class NAlarmWdg extends Widget
                 }
             }
             borkas.removeAll(forRemove);
+            Gob player = NUtils.player();
+            if(player!=null) {
+                for (Long id : borkas) {
+                    if (!alarms.contains(id)) {
+                        Gob gob = Finder.findGob(id);
+                        Buddy buddy = gob.getattr(Buddy.class);
+                        if (buddy == null) {
+                            addAlarm(id);
+                            synchronized (player.ols) {
+                                player.addol(new NDirArrow(NUtils.player(), Color.WHITE, 50, gob, null));
+                            }
+                        } else {
+                            if (buddy.b != null) {
+                                if (BuddyWnd.gc[buddy.b.group] == Color.WHITE || BuddyWnd.gc[buddy.b.group] == Color.RED) {
+                                    addAlarm(id);
+                                    synchronized (player.ols) {
+                                        player.addol(new NDirArrow(NUtils.player(), BuddyWnd.gc[buddy.b.group], 50, gob, null));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            forRemove.clear();
+            for(Long id: alarms) {
+                if(Finder.findGob(id)==null) {
+                    forRemove.add(id);
+                }
+            }
+            for(Long id: forRemove) {
+                alarms.remove(id);
+            }
+            if(!alarms.isEmpty()) {
+                numberAlarm = new TexI(active_title.render(String.valueOf(alarms.size())).img);
+            }
         }
+    }
+
+    public static boolean isInAlarm(Long id)
+    {
+        if(NUtils.getGameUI()==null)
+            return false;
+        if(NUtils.getGameUI().alarmWdg.alarms.contains(id))
+        {
+            Gob gob = Finder.findGob(id);
+            if(gob == null)
+                return false;
+            Buddy buddy = gob.getattr(Buddy.class);
+            if (buddy == null) {
+                return true;
+            } else {
+                if (buddy != null && buddy.b != null) {
+                    if (BuddyWnd.gc[buddy.b.group] == Color.WHITE || BuddyWnd.gc[buddy.b.group] == Color.RED) {
+                        return true;
+                    }
+                }
+                else
+                {
+                    NUtils.getGameUI().alarmWdg.alarms.remove(id);
+                    return false;
+                }
+            }
+        }
+        return false;
     }
 
     public static void addBorka(long id) {
@@ -54,11 +126,19 @@ public class NAlarmWdg extends Widget
         }
     }
 
+    private void addAlarm(Long id) {
+        synchronized (alarms) {
+            NAlarmManager.play("alarm/alarm");
+            alarms.add(id);
+        }
+    }
+
     @Override
     public void draw(GOut g) {
         if(!alarms.isEmpty()) {
             int id = (int) (NUtils.getTickId() / 5) % 12;
             g.image(NStyle.alarm[id], new Coord(sz.x / 2 - NStyle.alarm[0].sz().x / 2, sz.y / 2 - NStyle.alarm[0].sz().y / 2));
+            g.image(numberAlarm, new Coord(sz.x / 2 + UI.scale(12), sz.y / 2 - UI.scale(24)));
         }
         super.draw(g);
     }
