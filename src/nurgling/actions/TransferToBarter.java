a127a18bbc7f1a665673f1d42afbaabb8bccb594
Katodiy <katodiy@yandex.ru>
2024-10-19 23:38:38 +0300

    transferToBarter fix

--- src/nurgling/actions/TransferToBarter.java
+++ src/nurgling/actions/TransferToBarter.java
@@ -36,26 +36,22 @@ public class TransferToBarter implements Action{
         ArrayList<WItem> wItems = NUtils.getGameUI().getInventory().getItems(items,th);
         if(wItems.isEmpty())
             return Results.SUCCESS();
-        new PathFinder(barter.barter).run(gui);
-        new OpenTargetContainer("Barter Stand", barter.barter).run(gui);
+        while (!wItems.isEmpty()) {
+            new PathFinder(barter.barter).run(gui);
+            new OpenTargetContainer("Barter Stand", barter.barter).run(gui);
 
-        Window barter_wnd = gui.getWindow("Barter Stand");
-        if(barter_wnd==null)
-        {
-            return Results.ERROR("No Barter window");
-        }
+            Window barter_wnd = gui.getWindow("Barter Stand");
+            if (barter_wnd == null) {
+                return Results.ERROR("No Barter window");
+            }
 
-        for(Widget ch = barter_wnd.child; ch != null; ch = ch.next)
-        {
-            if (ch instanceof Shopbox)
-            {
-                Shopbox sb = (Shopbox) ch;
-                Shopbox.ShopItem price = sb.getPrice();
-                if (price != null)
-                {
-                    if (NParser.checkName(price, items))
-                    {
-                        while (!wItems.isEmpty()) {
+            for (Widget ch = barter_wnd.child; ch != null; ch = ch.next) {
+                if (ch instanceof Shopbox) {
+                    Shopbox sb = (Shopbox) ch;
+                    Shopbox.ShopItem price = sb.getPrice();
+                    if (price != null) {
+                        if (NParser.checkName(price, items)) {
+
                             int startSize = gui.getInventory().getItems("Branch").size();
                             int target_size = (sb.leftNum != 0) ? Math.min(wItems.size(), sb.leftNum) : wItems.size();
                             for (int i = 0; i < target_size; i++) {
@@ -65,8 +61,8 @@ public class TransferToBarter implements Action{
                             new PathFinder(barter.chest).run(gui);
                             new OpenTargetContainer("Chest", barter.chest).run(gui);
                             ArrayList<WItem> branchitems = gui.getInventory().getItems("Branch");
-                            new SimpleTransferToContainer(gui.getInventory("Chest"), gui.getInventory().getItems("Branch"), branchitems.size()-startSize).run(gui);
-                            wItems = NUtils.getGameUI().getInventory().getItems(items,th);
+                            new SimpleTransferToContainer(gui.getInventory("Chest"), gui.getInventory().getItems("Branch"), branchitems.size() - startSize).run(gui);
+                            wItems = NUtils.getGameUI().getInventory().getItems(items, th);
                         }
                     }
                 }
