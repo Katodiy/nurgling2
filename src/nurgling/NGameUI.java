3e114a5d5392125b627c42babb4d1402cd094b96
Katodiy <katodiy@yandex.ru>
2024-10-25 22:15:52 +0300

    quality icon

--- src/nurgling/NGameUI.java
+++ src/nurgling/NGameUI.java
@@ -8,6 +8,7 @@ import haven.res.ui.rbuff.*;
 import haven.res.ui.relcnt.RelCont;
 import nurgling.conf.*;
 import nurgling.notifications.*;
+import nurgling.overlays.QualityOl;
 import nurgling.tools.*;
 import nurgling.widgets.*;
 
@@ -16,6 +17,8 @@ import java.awt.event.KeyEvent;
 import java.util.*;
 import java.util.List;
 import java.util.function.Supplier;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static haven.Inventory.invsq;
 
@@ -613,5 +616,24 @@ public class NGameUI extends GameUI
         }
 
 
+    }
+
+    @Override
+    public void msg(String msg, Color color, Color logcol) {
+        if (msg.contains("Quality")) {
+            if(map.clickedGob!=null)
+            {
+                Matcher m = Pattern.compile("Quality: (\\d+)").matcher(msg);
+                if(m.matches()) {
+                    try {
+                        map.clickedGob.addcustomol(new QualityOl(map.clickedGob, Integer.parseInt(m.group(1))));
+                    } catch (NumberFormatException ignored) {
+                    } finally {
+                        map.clickedGob = null;
+                    }
+                }
+            }
+        }
+        super.msg(msg, color, logcol);
     }
 }
