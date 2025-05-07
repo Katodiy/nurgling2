6c1525a9436809a5c4d74c9eb86e469444bfccc7
Alex Svoboda <asvoboda@asu.edu>
2025-05-07 08:48:17 -0700

    feat/poppy farmer

--- src/nurgling/tools/SpecialisationData.java
+++ src/nurgling/tools/SpecialisationData.java
@@ -8,7 +8,7 @@ public class SpecialisationData {
     public final static HashMap<String, ArrayList<String>> data = new HashMap<>();
     static
     {
-        ArrayList<String> crops = new ArrayList<>(Arrays.asList("Flax", "Turnip", "Carrot", "Hemp", "Millet", "Wheat", "Barley"));
+        ArrayList<String> crops = new ArrayList<>(Arrays.asList("Flax", "Turnip", "Carrot", "Hemp", "Millet", "Wheat", "Barley", "Poppy"));
         data.put("crop",crops);
         data.put("seed",crops);
         ArrayList<String> fuel = new ArrayList<>(Arrays.asList("Branch", "Coal", "Block", "Log"));
