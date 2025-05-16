df234dd3f5b286d751638e79c0d81122aab6df72
Alex Svoboda <asvoboda@asu.edu>
2025-05-15 19:38:05 -0700

    Address comments

--- src/nurgling/tasks/WaitForDoorGob.java
+++ src/nurgling/tasks/WaitForDoorGob.java
@@ -1,36 +1,41 @@
 package nurgling.tasks;
 
 import haven.Gob;
-import nurgling.NUtils;
 import nurgling.tools.Finder;
 import nurgling.tools.NAlias;
+
+import java.util.ArrayList;
 
 public class WaitForDoorGob extends NTask {
 
     @Override
     public boolean check() {
-        Gob door;
-        try {
-            door = Finder.findGob(NUtils.player().rc, new NAlias(
-                    "gfx/terobjs/arch/stonestead",
-                    "gfx/terobjs/arch/stonemansion",
-                    "gfx/terobjs/arch/greathall",
-                    "gfx/terobjs/arch/primitivetent",
-                    "gfx/terobjs/arch/windmill",
-                    "gfx/terobjs/arch/stonetower",
-                    "gfx/terobjs/arch/logcabin",
-                    "gfx/terobjs/arch/timberhouse",
-                    "gfx/terobjs/arch/minehole",
-                    "gfx/terobjs/arch/ladder",
-                    "gfx/terobjs/arch/upstairs",
-                    "gfx/terobjs/arch/downstairs",
-                    "gfx/terobjs/arch/cellardoor",
-                    "gfx/terobjs/arch/cellarstairs"
-            ), null, 100);
-        } catch (Exception e) {
-            return false;
+        ArrayList<Gob> door;
+
+        String[] names = {
+                "gfx/terobjs/arch/stonestead",
+                "gfx/terobjs/arch/stonemansion",
+                "gfx/terobjs/arch/greathall",
+                "gfx/terobjs/arch/primitivetent",
+                "gfx/terobjs/arch/windmill",
+                "gfx/terobjs/arch/stonetower",
+                "gfx/terobjs/arch/logcabin",
+                "gfx/terobjs/arch/timberhouse",
+                "gfx/terobjs/arch/minehole",
+                "gfx/terobjs/arch/ladder",
+                "gfx/terobjs/arch/upstairs",
+                "gfx/terobjs/arch/downstairs",
+                "gfx/terobjs/arch/cellardoor",
+                "gfx/terobjs/arch/cellarstairs"
+        };
+
+        for(String name : names) {
+            door = Finder.findGobs(new NAlias(name));
+            if(door.size() > 0) {
+                return true;
+            }
         }
 
-        return door != null;
+        return false;
     }
 }
