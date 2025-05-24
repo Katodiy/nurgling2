b1b54d7c21898aafeaf7d8c48abe0975def56a90
Alex Svoboda <asvoboda@asu.edu>
2025-05-24 00:06:18 -0700

    add lettuce bot

--- src/nurgling/widgets/NBotsMenu.java
+++ src/nurgling/widgets/NBotsMenu.java
@@ -62,6 +62,7 @@ public class NBotsMenu extends Widget
 
         farming.elements.add(new NButton("hemp", new HempFarmer(), true));
         farming.elements.add(new NButton("flax", new FlaxFarmer(), true));
+        farming.elements.add(new NButton("lettuce", new LettuceFarmer(), true));
 
         farming.elements.add(new NButton("barley", new BarleyFarmer(), true));
         farming.elements.add(new NButton("millet", new MilletFarmer(), true));
