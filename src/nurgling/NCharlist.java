c8f343afcaab71afd7d024e9626f9cb05aaba1ef
Alex Svoboda <40924818+aleksandrsvoboda@users.noreply.github.com>
2025-06-18 22:27:15 -0700

    fix/remove second logout button (#66)

--- src/nurgling/NCharlist.java
+++ src/nurgling/NCharlist.java
@@ -1,20 +1,15 @@
 package nurgling;
-import haven.Button;
 import haven.Charlist;
-import haven.RemoteUI;
-import haven.UI;
 
 public class NCharlist extends Charlist {
     static NCharlist instance;
-
-    Button logout;
 
     public NCharlist(int height ) {
         super ( height );
         instance = this;
     }
 
-    public static void play(){
+    public static void play() {
         if(NConfig.botmod!=null && instance!=null) {
             for ( Char c: instance.chars) {
                 if(c.name.equals ( NConfig.botmod.character  ))
@@ -25,19 +20,6 @@ public class NCharlist extends Charlist {
                 }
             }
         }
-    }
-
-    protected void added() {
-        parent.setfocus(this);
-        logout = parent.add(new Button(UI.scale(90), "Log out") {
-            @Override
-            public void click() {
-                RemoteUI rui = (RemoteUI) ui.rcvr;
-                synchronized (rui.sess) {
-                    rui.sess.close();
-                }
-            }
-        }, UI.scale(121, 553));
     }
 
     @Override
