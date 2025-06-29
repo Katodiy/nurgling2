4061451a280cf6edada55020e532c93c123b1b48
Alex Svoboda <asvoboda@asu.edu>
2025-06-28 21:31:17 -0700

    Only harvest last stage when quality grinding

--- src/nurgling/actions/HarvestCrop.java
+++ src/nurgling/actions/HarvestCrop.java
@@ -310,10 +310,22 @@ public class HarvestCrop implements Action {
 
     private boolean hasAnyCropStage(NArea field, NAlias crop) throws InterruptedException {
         List<CropRegistry.CropStage> cropStages = CropRegistry.HARVESTABLE.getOrDefault(crop, Collections.emptyList());
-        for (CropRegistry.CropStage cs : cropStages) {
-            if (!Finder.findGobs(field, crop, cs.stage).isEmpty())
-                return true;
+
+        if (cropStages.isEmpty())
+            return false;
+
+        if (isQualityGrid) {
+            // Only check the last (latest) stage. This is done specifically for Turnip and Carrots (eventually maybe
+            // leek). We want the plant to get to the last stage to get the quality gain it requires.
+            CropRegistry.CropStage lastStage = cropStages.get(cropStages.size() - 1);
+            return !Finder.findGobs(field, crop, lastStage.stage).isEmpty();
+        } else {
+            for (CropRegistry.CropStage cs : cropStages) {
+                if (!Finder.findGobs(field, crop, cs.stage).isEmpty())
+                    return true;
+            }
         }
+
         return false;
     }
 
