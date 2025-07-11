beec5cfd54c9e2a2b753ac89ab4def01c9cbc8ee
Alex Svoboda <asvoboda@asu.edu>
2025-07-10 21:42:45 -0700

    Add bots

---
+++ src/nurgling/actions/bots/LeekFarmerQ.java
@@ -0,0 +1,65 @@
+package nurgling.actions.bots;
+
+import nurgling.NConfig;
+import nurgling.NGameUI;
+import nurgling.NInventory;
+import nurgling.NUtils;
+import nurgling.actions.*;
+import nurgling.areas.NArea;
+import nurgling.areas.NContext;
+import nurgling.tools.NAlias;
+import nurgling.widgets.Specialisation;
+
+import java.util.ArrayList;
+
+public class LeekFarmerQ implements Action {
+    @Override
+    public Results run(NGameUI gui) throws InterruptedException {
+        boolean oldStackingValue = ((NInventory) NUtils.getGameUI().maininv).bundle.a;
+
+        NArea.Specialisation cropQ = new NArea.Specialisation(Specialisation.SpecName.cropQ.toString(), "Leek");
+        NArea.Specialisation seedQ = new NArea.Specialisation(Specialisation.SpecName.seedQ.toString(), "Leek");
+        NArea.Specialisation trough = new NArea.Specialisation(Specialisation.SpecName.trough.toString());
+
+        NArea leekArea = NContext.findOut("Leek", 1);
+
+        if(leekArea == null) {
+            return Results.ERROR("PUT Area for Leek required, but not found!");
+        }
+
+        Boolean cleanupQContainers = (Boolean) NConfig.get(NConfig.Key.cleanupQContainers);
+
+        ArrayList<NArea.Specialisation> req = new ArrayList<>();
+        req.add(cropQ);
+        req.add(seedQ);
+        ArrayList<NArea.Specialisation> opt = new ArrayList<>();
+        opt.add(trough);
+
+        if (new Validator(req, opt).run(gui).IsSuccess()) {
+            NUtils.stackSwitch(true);
+
+            new HarvestCrop(
+                    NContext.findSpec(cropQ),
+                    NContext.findSpec(seedQ),
+                    null,
+                    new NAlias("plants/leek"),
+                    true
+            ).run(gui);
+
+            if (leekArea != null)
+                new CollectItemsToPile(NContext.findSpec(cropQ).getRCArea(), leekArea.getRCArea(), new NAlias("items/leek", "Leek")).run(gui);
+
+            new SeedCrop(NContext.findSpec(cropQ), NContext.findSpec(seedQ), new NAlias("plants/leek"), new NAlias("Leek"), false, true).run(gui);
+
+            if (cleanupQContainers && NContext.findSpec(trough) != null) {
+                new CleanupSeedQContainer(NContext.findSpec(seedQ), new NAlias("Leek"), NContext.findSpec(trough)).run(gui);
+            }
+
+            NUtils.stackSwitch(oldStackingValue);
+            return Results.SUCCESS();
+        }
+
+        NUtils.stackSwitch(oldStackingValue);
+        return Results.FAIL();
+    }
+}
