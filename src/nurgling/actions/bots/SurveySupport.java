5300ec080e85c4d1fdd6670befacdaa0e0292225
Katodiy <katodiy@yandex.ru>
2025-07-13 15:59:16 +0300

    survey supporter fixes

--- src/nurgling/actions/bots/SurveySupport.java
+++ src/nurgling/actions/bots/SurveySupport.java
@@ -2,16 +2,17 @@ package nurgling.actions.bots;
 
 import haven.*;
 import haven.res.ui.relcnt.RelCont;
+import nurgling.NConfig;
 import nurgling.NGameUI;
+import nurgling.NMapView;
 import nurgling.NUtils;
 import nurgling.actions.Action;
 import nurgling.actions.PathFinder;
 import nurgling.actions.RestoreResources;
 import nurgling.actions.Results;
-import nurgling.tasks.GetCurs;
-import nurgling.tasks.NTask;
-import nurgling.tasks.WaitPose;
-import nurgling.tasks.WaitWindow;
+import nurgling.areas.NContext;
+import nurgling.routes.RoutePoint;
+import nurgling.tasks.*;
 import nurgling.tools.Finder;
 import nurgling.tools.NAlias;
 import nurgling.tools.NParser;
@@ -20,26 +21,38 @@ public class SurveySupport implements Action
 {
     @Override
     public Results run(NGameUI gui) throws InterruptedException {
+
+
         Gob target = Finder.findGob(new NAlias("survobj"));
         if (target == null) {
             return Results.ERROR("Survey object not found");
         }
+        long targetid = target.id;
+        new PathFinder(target.rc).run(gui);
+        RoutePoint rp = ((NMapView) NUtils.getGameUI().map).routeGraphManager.getGraph().findNearestPointToPlayer(NUtils.getGameUI());
+
 
 
         String leftover = null;
 
-        new PathFinder(target.rc).run(gui);
+
         while (true) {
 
             if (!new RestoreResources().run(gui).IsSuccess()) {
                 return Results.FAIL();
             }
-            target = Finder.findGob(target.id);
+            if(rp!=null && rp.toCoord2d(NUtils.getGameUI().map.glob.map)==null)
+                new RoutePointNavigator(rp).run(gui);
+            target = Finder.findGob(targetid);
+            if(target == null)
+                return Results.ERROR("Survey object not found");
+            new PathFinder(target.rc).run(gui);
             NUtils.rclickGob(target);
             NUtils.addTask(new WaitWindow("Land survey"));
 
             Window wnd = NUtils.getGameUI().getWindow("Land survey");
             if (wnd != null) {
+                boolean needRemove = false;
                 for (Widget child : wnd.children()) {
                     if (child instanceof Label) {
                         Label cand = (Label) child;
@@ -53,7 +66,7 @@ public class SurveySupport implements Action
                             if (leftover == null || !leftover.equals(cand.text())) {
                                 leftover = cand.text();
                             } else {
-                                return Results.SUCCESS();
+                                needRemove = true;
                             }
                         }
                     }
@@ -63,8 +76,23 @@ public class SurveySupport implements Action
                         if (button.text.text.equals("Dig")) {
                             button.click();
                         }
+                        if (needRemove)
+                            if (button.text.text.equals("Remove")) {
+                                button.click();
+                                NUtils.addTask(new WindowIsClosed(wnd));
+                                target = Finder.findGob(new NAlias("survobj"));
+                                if (target == null) {
+                                    return Results.ERROR("Survey object not found");
+                                }
+                                targetid = target.id;
+                                new PathFinder(target.rc).run(gui);
+                                rp = ((NMapView) NUtils.getGameUI().map).routeGraphManager.getGraph().findNearestPointToPlayer(NUtils.getGameUI());
+                                break;
+                            }
                     }
                 }
+                if(needRemove)
+                    continue;
                 Gob player = NUtils.player();
                 if (player == null) {
                     return Results.FAIL();
