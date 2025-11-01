da7edc8a98f095ec39de8789633f488365484e5f
Katodiy <katodiy@yandex.ru>
2025-11-01 23:49:09 +0300

    smoking fix

--- src/nurgling/widgets/SmokingSettings.java
+++ src/nurgling/widgets/SmokingSettings.java
@@ -76,11 +76,14 @@ public class SmokingSettings extends Window implements Checkable {
             for(NSmokProp p : smprop)
             {
                 JSONObject icon = (JSONObject)((JSONObject) p.toJson().get("data")).get("icon");
-                IconItem item = new IconItem(p.iconName, ItemTex.create(icon), this);
-                item.src = icon;
-                Recipe recipe = new Recipe(item,(String) ((JSONObject) p.toJson().get("data")).get("fuel"));
-                recipe.isEnabled.a = (Boolean) ((JSONObject) p.toJson().get("data")).get("isSelected");
-                recipes.add(recipe);
+                if(icon!=null)
+                {
+                    IconItem item = new IconItem(p.iconName, ItemTex.create(icon), this);
+                    item.src = icon;
+                    Recipe recipe = new Recipe(item, (String) ((JSONObject) p.toJson().get("data")).get("fuel"));
+                    recipe.isEnabled.a = (Boolean) ((JSONObject) p.toJson().get("data")).get("isSelected");
+                    recipes.add(recipe);
+                }
             }
         }
     }
