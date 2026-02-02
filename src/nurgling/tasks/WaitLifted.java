6710ec838a5f2af5b86d594e8ed47d29ed94dc38
Xao <timur962@gmail.com>
2026-02-02 13:52:17 +0500

    fix/WaitLifted get gob by id

--- src/nurgling/tasks/WaitLifted.java
+++ src/nurgling/tasks/WaitLifted.java
@@ -4,21 +4,26 @@ import haven.Coord2d;
 import haven.Following;
 import haven.Gob;
 import nurgling.NUtils;
+import nurgling.tools.Finder;
 
 public class WaitLifted extends NTask {
-    Gob gob;
+    private final long gobid;
 
     public WaitLifted(Gob gob) {
-        this.gob = gob;
+        this.gobid = (gob == null) ? -1 : gob.id;
+        this.maxCounter = 50;
     }
-
 
     @Override
     public boolean check() {
-        Following fl;
-        return (fl = gob.getattr(Following.class))!=null && fl.tgt == NUtils.playerID();
+        if (gobid < 0)
+            return false;
+        Gob gob = Finder.findGob(gobid);
+        if (gob == null)
+            return false;
+        Following fl = gob.getattr(Following.class);
+        return (fl != null) && (fl.tgt == NUtils.playerID());
     }
-
 }
 
 
